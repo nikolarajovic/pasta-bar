267734eaf83a131bb43a6172503b91fedf591780
Nikola Rajovic <nikola.rajovic1992@gmail.com>
2021-12-30 13:47:42 +0100

    Is regular customer method

--- src/mini_projekat_2022/pasta_bar.java
+++ src/mini_projekat_2022/pasta_bar.java
@@ -15,6 +15,15 @@ public class pasta_bar {
 		return findIngredient;
 	}
 	
+	public static boolean isRegularCustomer(String[] regularCustomers, String customer) {
+		for (int i = 0; i < regularCustomers.length; i++) {
+			if (regularCustomers[i].equals(customer)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 
 		Scanner s = new Scanner(System.in);
@@ -27,9 +36,7 @@ public class pasta_bar {
 								"Napolitana", "Vegetable mix", "Mushrooms", "Box" };
 		int[] prices = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
 		
-		System.out.print("Ingredient: ");
-		String ingredient = s.nextLine();
-
+	
 	}
 
 }
